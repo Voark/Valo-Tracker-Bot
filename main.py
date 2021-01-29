399f4a8fe8d3680693d236d68663509ee50b3200
Voark <fredisawesome147@gmail.com>
2021-01-28 18:29:46 -0800

    Main functions working

--- main.py
+++ main.py
@@ -2,18 +2,11 @@ import discord
 from discord.ext import commands
 from bs4 import BeautifulSoup
 from util import *
-import urllib.parse
-import requests
+from json import *
 
 f = open('api_key.txt')
 api_key = f.read()
 f.close()
-
-class NotSignedInException(Exception):
-    pass
-
-class NoAccountException(Exception):
-    pass
 
 client = discord.Client()
 
@@ -24,14 +17,60 @@ async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
 @bot.command(name = 'track')
-async def track(ctx, *, ID):
-    URL = generate_url(ID)
-    soup = open_url(URL) 
+async def track_id(ctx, *, ID):
+    ID = ID.replace(' ', '')
     try:
-        await ctx.send(process_soup(soup))
+        await ctx.send(rank_from_id(ID))
     except NotSignedInException:
         await ctx.send('Sign in to your account here:')
     except NoAccountException:
         await ctx.send('Account doesn\'t exist')
+    
+@bot.command(name = 'add')
+async def add_id(ctx, *, ID):
+    ID = ID.replace(' ', '')
+    guild_id = ctx.guild.id
+    try:
+        rank, url = rank_from_id(ID, return_url = True)
+        data = open_json(guild_id)
+        if data == None:
+            data = dict()
+        data[ID] = {'rank': rank, 'link': url}
+        write_json(data, guild_id)
+        await ctx.send(f'Successfully added {ID}')
+    except NotSignedInException:
+        await ctx.send(f'Unprivate your profile. {generate_url(ID)}')
+    except NoAccountException:
+        await ctx.send('Account doesn\'t exist')
+
+@bot.command(name = 'remove')
+async def remove_id(ctx, *, ID):
+    guild_id = ctx.guild.id
+    try:
+        data = open_json(guild_id)
+        del data[ID]
+        write_json(data, guild_id)
+        await ctx.send(f'Successfully removed {ID}')
+    except:
+        await ctx.send('Account not on list.')
+
+@bot.command(name = 'list')
+async def list_ids(ctx):
+    guild_id = ctx.guild.id
+    await reload_json(guild_id)
+    data = open_json(guild_id)
+    if not data:
+        await ctx.send('No accounts to track. Add accounts using \'.add\'')
+        return
+    await ctx.message.delete()
+    async for x in ctx.message.channel.history(limit = 2):
+        if x.author == bot.user:
+            await x.delete()
+    formatted_usernames = '\n'.join([f'[{key}]({value["link"]})' for key, value in sorted(data.items(), key = lambda x: rank_to_int(x[1]['rank']), reverse = True)])
+    formatted_ranks = '\n'.join([rank['rank'] for rank in sorted(data.values(), key = lambda x: rank_to_int(x['rank']), reverse = True)])
+    embed = discord.Embed(title="Accounts")
+    embed.add_field(name = "Username", value = formatted_usernames, inline = True)
+    embed.add_field(name = "Rank", value = formatted_ranks, inline = True)
+    await ctx.send(embed = embed)
 
 bot.run(api_key)
--- util.py
+++ util.py
@@ -1,13 +1,39 @@
+import urllib.parse
+import requests
+import json
+from bs4 import BeautifulSoup
+import asyncio
+
+RANKS = {'Iron 1'    : 0 , 'Iron 2'    : 1 , 'Iron 3'    : 2 , 
+         'Bronze 1'  : 3 , 'Bronze 2'  : 4 , 'Bronze 3'  : 5 ,
+         'Silver 1'  : 6 , 'Silver 2'  : 7 , 'Silver 3'  : 8 ,
+         'Gold 1'    : 9 , 'Gold 2'    : 10, 'Gold 3'    : 11,
+         'Platinum 1': 12, 'Platinum 2': 13, 'Platinum 3': 14,
+         'Diamond 1' : 15, 'Diamond 2' : 16, 'Diamond 3' : 17,
+         'Immortal 1': 18, 'Immortal 2': 19, 'Immortal 3': 20,
+         'Radiant '  : 69, 'Error': -2, 'Unrated': -1}
+
+# Exceptions
+class NotSignedInException(Exception):
+    pass
+
+class NoAccountException(Exception):
+    pass
+
+def rank_to_int(rank):
+    return RANKS[rank]
+
 def generate_url(ID):
     return f'https://tracker.gg/valorant/profile/riot/{urllib.parse.quote(ID)}/overview?playlist=competitive'
 
 def open_url(URL) -> 'soup':
+    # TODO: Handle rate limit/no response
     page = requests.get(URL)
     return BeautifulSoup(page.content, 'html.parser')
 
 def find_rank(soup) -> 'rank':
     search = soup.find('div', class_ = 'valorant-rank-bg')
-    return search.text
+    return search.text.strip()
 
 def process_soup(soup):
     error = soup.find('p', class_ = 'error-message')
@@ -18,3 +44,43 @@ def process_soup(soup):
             raise NoAccountException
     else:
         return find_rank(soup)
+
+def rank_from_id(ID, return_url = False):
+    URL = generate_url(ID)
+    soup = open_url(URL) 
+    if return_url:
+        return process_soup(soup), URL
+    return process_soup(soup)
+
+# {ID: {'link': str, 'rank': int}}
+
+def open_json(server_id):
+    try:
+        with open('data/' + str(server_id) + '.json', 'r') as file:
+            data = json.load(file)
+    except:
+        return None
+    return data
+
+def write_json(data, server_id):
+    with open('data/' + str(server_id) + '.json', 'wt') as file:
+        json.dump(data, file)
+
+async def reload_json(server_id):
+    data = open_json(server_id)
+    if data == None:
+        return 
+    for ID in data:
+        try:
+            rank, url = rank_from_id(ID, return_url = True)
+        except:
+            rank = 'Error'
+            url = generate_url(ID)
+        await asyncio.sleep(2)
+        data[ID] = {'rank': rank, 'link': url}
+    write_json(data, server_id)
+
+if __name__ == '__main__':
+    write_json({'a': 'b', 'c': 'd'}, 1)
+    print(open_json(1))
+    print(open_json(1))
